f261e3cab5518c0475209643d16f1a789a503c95
aseruneko <aseruneko@gmail.com>
2020-05-28 18:29:13 +0900

    lesson08

---
+++ lesson08.sql
@@ -0,0 +1,10 @@
+-- insert into users (id, name, score) values (1, "taguchi", 5.8);
+-- insert into users (id, name, score) values (2, "fkoji", 8.2);
+-- insert into users (id, name, score) values (3, "dotinstall", 6.1);
+-- insert into users (id, name, score) values (4, "yamada", null);
+
+insert into users (id, name, score) values
+    (1, "taguchi", 5.8),
+    (2, "fkoji", 8.2);
+
+select * from users;
